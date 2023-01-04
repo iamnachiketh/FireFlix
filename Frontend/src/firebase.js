9f0e41220fc2cd1f216214c1f154231dc804c7a2
nachiketha.k.c <nachikethkc@gmail.com>
2023-01-04 21:03:16 +0530

    removed some files

--- Frontend/src/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDwcy7XJobEwDyBYYGXi9STn-sZUjeJKnA",
-    authDomain: "netflix-clone-9c77e.firebaseapp.com",
-    projectId: "netflix-clone-9c77e",
-    storageBucket: "netflix-clone-9c77e.appspot.com",
-    messagingSenderId: "61870998116",
-    appId: "1:61870998116:web:b9db8a49791f4d9fb45571",
-    measurementId: "G-BQ89H5J295"
-  };
